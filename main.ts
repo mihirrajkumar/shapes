728b7463f1d5305275566d60ebf0911f7a620640
mihirrajkumar <77078237+mihirrajkumar@users.noreply.github.com>
2021-01-06 15:45:55 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,207 @@
+basic.forever(function () {
+    basic.showString("shapes")
+    basic.showString("circle")
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showString("line")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("curve")
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showString("triangle")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        . . . . .
+        `)
+    basic.showString("square")
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("rectangle")
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # . . . #
+        # # # # #
+        . . . . .
+        `)
+    basic.showString("diamond")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showString("pentagon")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("hexagon")
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showString("heptagon")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . # .
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("octagon")
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # . # .
+        . # # # .
+        `)
+    basic.showString("nonagon")
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . . # .
+        . # # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showString("decagon")
+    basic.showLeds(`
+        . # # . .
+        # . . # #
+        # . . . #
+        # # # # #
+        . . . . .
+        `)
+    basic.showString("oval")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . # . .
+        `)
+    basic.showString("heart")
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showString("star")
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        # . . . #
+        # . . . #
+        `)
+    basic.showString("semi-circle")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("trapezoid")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("parrellogram")
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . # . . #
+        # # # # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showString("point")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("cross")
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.showString("dart")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        # # . # #
+        . . . . .
+        `)
+    basic.showString("kite")
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showString("rhombus")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showString("cresent")
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . # . .
+        # . . # .
+        . # # . .
+        `)
+})
